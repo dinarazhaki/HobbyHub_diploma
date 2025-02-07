3d0be8034017e1eddeee347ec6b85059e76397cf
Dinara Zhaki <210103379@stu.sdu.edu.kz>
2025-02-07 15:03:20 +0500

    Week 4

--- hobbyhub/events/urls.py
+++ hobbyhub/events/urls.py
@@ -5,5 +5,13 @@ urlpatterns = [
     path('guest/',guest_page),
     path('organizer/',organizer_view),
     path('user/',user_view),
-
+    path('user_profile/',user_profile), #!!!!need to update
+    path('user_profile/user_activities/',profile_user_act), #!!!!need to update
+    path('user_profile/user_achievements/',user_achievements), #!!!!need to update
+    path('user_profile/setting',user_setting), #!!!!need to update
+    path('user_profile/language',user_language), #!!!!need to update
+    path('user_profile/notification_preferences',user_notification), #!!!!need to update
+    path('user_profile/privacy',user_privacy), #!!!!need to update
+    
+    
 ]
--- hobbyhub/events/views.py
+++ hobbyhub/events/views.py
@@ -22,3 +22,25 @@ def user_view(request):
     events_data = json.loads(events_json)
     
     return render(request, 'user.html', {'events': events, 'events_data': events_data})
+
+def user_profile(request):  #!!!!need to update
+    return render(request,'userprofile.html')
+
+
+def profile_user_act(request):  #!!!!need to update
+    return render(request,'profile_user_act.html')
+
+def user_achievements(request):  #!!!!need to update
+    return render(request,'user_achievements.html')
+
+def user_setting(request):  #!!!!need to update
+    return render(request,'user_setting.html')
+
+def user_language(request):  #!!!!need to update
+    return render(request,'language_options.html')
+
+def user_notification(request):  #!!!!need to update
+    return render(request,'notif_preferences.html')
+
+def user_privacy(request):  #!!!!need to update
+    return render(request,'privacy_settings.html')
