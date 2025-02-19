0d9d10feedcff4f38274a8c36e938b74888d4ada
Symbat Mekembayeva <msymbat28@gmail.com>
2025-02-19 21:32:20 +0500

    Creation part is added

--- back_hobbyhub/events/static/js/activity_details.js
+++ back_hobbyhub/events/static/js/activity_details.js
@@ -8,15 +8,10 @@ L.marker([43.2389, 76.8897]).addTo(map)
     .bindPopup('office, Almaty')
     .openPopup();
 
- let gameCode = Math.floor(100000 + Math.random() * 900000).toString();
-
-// Get the container element
+let gameCode = Math.floor(100000 + Math.random() * 900000).toString();
 let codeBox = document.getElementById("gameCode");
-
-// Clear any previous content (if needed)
 codeBox.innerHTML = "";
 
-// Splitting the code into separate spans
 gameCode.split("").forEach(num => {
     let span = document.createElement("span");
     span.innerText = num;
@@ -28,12 +23,9 @@ function toggleDivs() {
     let firstDivs = document.getElementsByClassName("challenge-info");
     let secondDivs = document.getElementsByClassName("secondContainer");
 
-    // Hide all elements with class "challenge-info"
     for (let i = 0; i < firstDivs.length; i++) {
         firstDivs[i].style.display = "none";
     }
-
-    // Show all elements with class "seconContainer"
     for (let i = 0; i < secondDivs.length; i++) {
         secondDivs[i].classList.remove("hidden");
     }
