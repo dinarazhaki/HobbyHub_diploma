0296c189c2c080b31adc72c6e0f0f8d420373422
Symbat Mekembayeva <msymbat28@gmail.com>
2025-03-03 03:00:22 +0500

    timer

--- back_hobbyhub/events/static/js/leaderboard.js
+++ back_hobbyhub/events/static/js/leaderboard.js
@@ -13,4 +13,30 @@ document.addEventListener("DOMContentLoaded", function () {
             row.style.opacity = 1;
         }, index * 100);
     });
+
+
+    updateCountdown();
+    
+
+
 });
+
+function updateCountdown() {
+    let now = new Date();
+    let lastDay = new Date(now.getFullYear(), now.getMonth() + 1, 0, 23, 59, 59);
+    let diff = lastDay - now;
+
+    if (diff <= 0) {
+        document.getElementById("countdown").innerHTML = "Time's up!";
+        return;
+    }
+    let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+            let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+            let seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+            document.getElementById("countdown").innerHTML = 
+                `${days}d ${hours}h ${minutes}m ${seconds}s`;
+
+            setTimeout(updateCountdown, 1000);
+}
