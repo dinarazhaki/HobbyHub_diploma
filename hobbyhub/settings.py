e4443d34b49215b25e4059f212f62b20ebb00635
RautARON <200103442@stu.sdu.edu.kz>
2025-04-23 03:07:11 +0500

    2.5

--- hobbyhub/settings.py
+++ hobbyhub/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', default=False, cast=bool)
+DEBUG = os.getenv('DEBUG', 'False')=='True'
 
 ALLOWED_HOSTS =['*']
 # ['localhost', '127.0.0.1', '192.168.3.30', 'hobbyhub.com', 'www.hobbyhub.com']
