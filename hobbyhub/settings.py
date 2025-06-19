78fb3653b161d884098704045db29b30a164002f
RautARON <200103442@stu.sdu.edu.kz>
2025-06-20 02:41:13 +0500

    cloud_2.0

--- hobbyhub/settings.py
+++ hobbyhub/settings.py
@@ -230,8 +230,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 AUTH_USER_MODEL = "events.Company"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = '/'
