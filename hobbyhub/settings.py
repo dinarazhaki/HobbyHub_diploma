2eb55afed96dcbbacef0405d7d69730b7c7d1ccf
RautARON <200103442@stu.sdu.edu.kz>
2025-06-20 02:33:08 +0500

    cloud

--- hobbyhub/settings.py
+++ hobbyhub/settings.py
@@ -74,6 +74,8 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
+    'cloudinary',
+    'cloudinary_storage',
 ]
 
 SOCIALACCOUNT_PROVIDERS= {
@@ -155,7 +157,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'hobbyhub.wsgi.application'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': config('CLOUDINARY_API_KEY'),
+    'API_SECRET': config('CLOUDINARY_API_SECRET'),
+}
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
